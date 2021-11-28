61ae5cbb7da706503e5edaa90706965fcda3c0d9
Benjamin Watson <benjamin.watson.1995@gmail.com>
2021-11-28 16:05:54 +0000

    Aws able to run handler, no longer confused by imports, getting data from event

--- response-lambda/index.js
+++ response-lambda/index.js
@@ -1,6 +1,5 @@
-import dotenv from 'dotenv';
-dotenv.config();
-import { Pool } from 'pg';
+require('dotenv').config();
+const { Pool } = require('pg');
 
 const pool = new Pool({
   user: process.env.DB_USER,
@@ -8,11 +7,12 @@ const pool = new Pool({
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT
-})
+});
 
-export async function handler(event) {
-  const query = 'INSERT INTO public.responses("imageId", modifier, "passedTest", "sessionId") VALUES($1, $2, $3, $4) RETURNING *'
-  const values = ['1', 1, true, "abc123"]
+exports.handler = async (event) => {
+  const { imageId, modifier, passedTest, sessionId } = event.body;
+  const query = 'INSERT INTO public.responses("imageId", modifier, "passedTest", "sessionId") VALUES($1, $2, $3, $4) RETURNING *';
+  const values = [imageId, modifier, passedTest, sessionId];
   const client = await pool.connect();
   try {
     const res = await client.query(query, values);
@@ -22,4 +22,4 @@ export async function handler(event) {
   } finally {
     client.release();
   }
-}
+};
